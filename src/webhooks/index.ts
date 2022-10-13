1eb8cf74cc02ae914a5ed578b4ef1f3a3a5a4f11
Maciej Wójcik <95306882+maciejjwojcik@users.noreply.github.com>
2022-10-13 11:26:07 +0200

    API-11469 lc-sdk-js neo ws (#70)
    * API-11469 added neo WS

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -190,13 +190,17 @@ interface GroupAssignment {
   priority: number;
 }
 
-type WorkScheduler = Record<
-  string,
-  {
-    start: string;
-    end: string;
-  }
->;
+interface WorkScheduler {
+  timezone: string;
+  schedule: Schedule[];
+}
+
+interface Schedule {
+  day: string;
+  enabled: boolean;
+  start: string;
+  end: string;
+}
 
 export interface AgentCreated {
   id: string;
