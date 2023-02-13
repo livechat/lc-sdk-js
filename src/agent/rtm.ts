bc3d2ccebebc09eb95b6d2512db75972080c2ff9
Maciek Wojcik <maciekwojcik1@gmail.com>
2023-02-13 14:45:21 +0100

    API-12128 add RequestEvent types

--- src/agent/rtm.ts
+++ src/agent/rtm.ts
@@ -33,6 +33,7 @@ import type {
   TransferChatParameters,
 } from "./structures";
 import { RTMAPI } from "../internal";
+import { RequestEvent } from "./structures/requestEvents";
 
 export default class RTM extends RTMAPI {
   constructor(options?: RTMAPIOptions) {
@@ -230,7 +231,7 @@ export default class RTM extends RTMAPI {
    * @param event - Event object
    * @param attach_to_last_thread - if true, adds event to last inactive thread
    */
-  async sendEvent(chat_id: string, event: Event, attach_to_last_thread?: boolean): Promise<SendEventResponse> {
+  async sendEvent(chat_id: string, event: RequestEvent, attach_to_last_thread?: boolean): Promise<SendEventResponse> {
     return this.send("send_event", {
       chat_id,
       event,
