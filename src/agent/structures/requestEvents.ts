22fb022d91a287c0ab66b75346fb5aa954a00cb3
Maciek Wojcik <maciekwojcik1@gmail.com>
2023-02-14 10:14:23 +0100

    API-12128 define types via omit

--- src/agent/structures/requestEvents.ts
+++ src/agent/structures/requestEvents.ts
@@ -1,46 +1,17 @@
-import { Properties } from "./structures";
-import { FormField, Postback, Element } from "./events";
+import { BaseEvent, File, FilledForm, Message, RichMessage, CustomEvent, SystemMessage } from "./events";
 
-interface RequestBaseEvent {
-  properties?: Properties;
-  visibility?: string;
-  type: string;
-}
+type RequestToOmit = Pick<BaseEvent, "id" | "created_at" | "visibility">
 
-export interface RequestFile extends RequestBaseEvent {
-  custom_id?: string;
-  name: string;
-  url: string;
-  alternative_text?: string;
-}
+export type RequestFile = Omit <File, keyof RequestToOmit | "author_id" | "thumbnail_url" | "thumbnail2x_url" | "content_type" | "size" | "width" | "height"> & Partial<Pick<File, "visibility">>
 
-export interface RequestFilledForm extends RequestBaseEvent {
-  custom_id?: string;
-  form_id: string;
-  fields: FormField[];
-}
+export type RequestFilledForm = Omit <FilledForm, keyof RequestToOmit | "author_id"> & Partial<Pick<FilledForm, "visibility">>
 
-export interface RequestMessage extends RequestBaseEvent {
-  custom_id?: string;
-  text: string;
-  postback?: Postback;
-}
+export type RequestMessage = Omit <Message, keyof RequestToOmit | "author_id"> & Partial<Pick<Message, "visibility">>
 
-export interface RequestRichMessage extends RequestBaseEvent {
-  custom_id?: string;
-  template_id: string;
-  elements?: Element[];
-}
+export type RequestRichMessage = Omit <RichMessage,keyof RequestToOmit | "author_id"> & Partial<Pick<RichMessage, "visibility">>
 
-export interface RequestCustomEvent extends RequestBaseEvent {
-  custom_id?: string;
-  content?: object;
-}
+export type RequestCustomEvent = Omit <CustomEvent,keyof RequestToOmit | "author_id"> & Partial<Pick<CustomEvent, "visibility">>
 
-export interface RequestSystemMessage extends RequestBaseEvent {
-  system_message_type: string;
-  text?: string;
-  text_vars?: object;
-}
+export type RequestSystemMessage = Omit <SystemMessage, keyof RequestToOmit | "author_id"> & Partial<Pick<SystemMessage, "visibility">>
 
 export type RequestEvent = RequestFile | RequestFilledForm | RequestMessage | RequestRichMessage | RequestCustomEvent | RequestSystemMessage;
