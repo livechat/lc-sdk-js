678d7b0c7e91411d1e45d40f6f087f5f3ab35f83
Maciek Wojcik <maciekwojcik1@gmail.com>
2023-02-14 12:58:55 +0100

    API-12128 remove join with properties

--- src/customer/structures/requestEvents.ts
+++ src/customer/structures/requestEvents.ts
@@ -2,16 +2,16 @@ import { BaseEvent, File, FilledForm, Message, RichMessage, CustomEvent, SystemM
 
 type RequestToOmit = Pick<BaseEvent, "id" | "created_at" | "recipients">
 
-export type RequestFile = Omit <File, keyof RequestToOmit | "author_id" | "thumbnail_url" | "thumbnail2x_url" | "content_type" | "size" | "width" | "height"> & Partial<Pick<File, "recipients">>
+export type RequestFile = Omit <File, keyof RequestToOmit | "author_id" | "thumbnail_url" | "thumbnail2x_url" | "content_type" | "size" | "width" | "height">
 
-export type RequestFilledForm = Omit <FilledForm, keyof RequestToOmit | "author_id"> & Partial<Pick<FilledForm, "recipients">>
+export type RequestFilledForm = Omit <FilledForm, keyof RequestToOmit | "author_id">
 
-export type RequestMessage = Omit <Message, keyof RequestToOmit | "author_id"> & Partial<Pick<Message, "recipients">>
+export type RequestMessage = Omit <Message, keyof RequestToOmit | "author_id">
 
-export type RequestRichMessage = Omit <RichMessage,keyof RequestToOmit | "author_id"> & Partial<Pick<RichMessage, "recipients">>
+export type RequestRichMessage = Omit <RichMessage,keyof RequestToOmit | "author_id">
 
-export type RequestCustomEvent = Omit <CustomEvent,keyof RequestToOmit | "author_id"> & Partial<Pick<CustomEvent, "recipients">>
+export type RequestCustomEvent = Omit <CustomEvent,keyof RequestToOmit | "author_id">
 
-export type RequestSystemMessage = Omit <CustomEvent, keyof RequestToOmit | "author_id"> & Partial<Pick<SystemMessage, "recipients">>
+export type RequestSystemMessage = Omit <CustomEvent, keyof RequestToOmit | "author_id">
 
 export type RequestEvent = RequestFile | RequestFilledForm | RequestMessage | RequestRichMessage | RequestCustomEvent | RequestSystemMessage;
