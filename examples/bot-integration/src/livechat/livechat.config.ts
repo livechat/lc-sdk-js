5f5afdd068bfe6d171cd2435c3042c9872e2bca9
Wojciech Fabjańczuk <w.fabjanczuk@livechat.com>
2023-02-20 16:03:09 +0100

    API-12101 improve sdk usage examples v3.3 (#104)
    * API-12101 improve sdk usage examples
    
    * API-12101 update sdk dependency in examples
    
    * API-12101 add missing empty line xd

---
+++ examples/bot-integration/src/livechat/livechat.config.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LivechatConfig {
+  public readonly licenseId: number;
+  public readonly clientId: string;
+  public readonly clientSecret: string;
+  public readonly webhookSecret: string;
+  public readonly baseApiUrl: string;
+  public readonly codeExchangeUrl: string;
+  public readonly redirectUri: string;
+
+  constructor(private configService: ConfigService) {
+    this.licenseId = configService.get<number>('LICENSE_ID');
+    this.clientId = configService.get('CLIENT_ID');
+    this.clientSecret = configService.get('CLIENT_SECRET');
+    this.webhookSecret = configService.get('WEBHOOK_SECRET');
+    this.baseApiUrl = configService.get('BASE_API_URL');
+    this.codeExchangeUrl = configService.get('ACCOUNTS_URL') + '/token';
+    this.redirectUri = configService.get('APP_URL') + '/install';
+  }
+}
---
+++ examples/rtm-messages/index.ts
@@ -0,0 +1,27 @@
+import { Agent, Customer } from "@livechat/lc-sdk-js";
+
+const { IncomingEvent } = Agent.Objects.Pushes;
+
+const agentAPI = new Agent.RTM();
+const customerAPI = new Customer.RTM(1234567890);
+
+(async () => {
+  Promise.all([agentAPI.connect(), customerAPI.connect()])
+    .then(() => Promise.all([agentAPI.login("Bearer dal:token"), customerAPI.login("Bearer dal:token")]))
+    .then(async () => {
+      const { chat_id } = await customerAPI.startChat();
+
+      agentAPI.on(IncomingEvent, () => {
+        agentAPI.sendEvent(chat_id, {
+          type: "message",
+          text: "agent msg 1",
+        });
+      });
+
+      await customerAPI.sendEvent(chat_id, {
+        type: "message",
+        text: "customer msg 1",
+      });
+    })
+    .then(() => agentAPI.logout());
+})();
