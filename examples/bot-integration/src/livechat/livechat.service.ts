1fa827f3832d9aabf2c4327c7999648adbf27300
Wojciech Fabjańczuk <w.fabjanczuk@livechat.com>
2023-02-20 16:33:29 +0100

    API-12101 improve sdk usage examples v3.6 (#107)
    * API-12101 improve sdk usage examples
    
    * API-12101 update sdk dependency in examples

---
+++ examples/bot-integration/src/livechat/livechat.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { LivechatConfig } from './livechat.config';
+import { AccountsService } from './accounts.service';
+import { Agent, Configuration } from '@livechat/lc-sdk-js';
+import AgentRtm from '@livechat/lc-sdk-js/lib/src/agent/rtm';
+import ConfigurationApi from '@livechat/lc-sdk-js/lib/src/configuration';
+
+@Injectable()
+export class LivechatService {
+  public botId: string;
+  public configurationWeb: ConfigurationApi;
+  public agentRtm: AgentRtm;
+
+  constructor(
+    private cfg: LivechatConfig,
+    private accountsService: AccountsService,
+  ) {}
+
+  async initialize(authorizationCode: string): Promise<void> {
+    const tokenGetter = await this.accountsService.exchangeAuthorizationCode(
+      authorizationCode,
+    );
+
+    this.configurationWeb = new Configuration.Web(
+      this.cfg.clientId,
+      tokenGetter,
+      { apiUrl: this.cfg.baseApiUrl },
+    );
+
+    this.agentRtm = new Agent.RTM({
+      apiUrl: this.cfg.baseApiUrl,
+    });
+    await this.agentRtm.connect();
+    await this.agentRtm.login(`Bearer ${tokenGetter().accessToken}`);
+  }
+
+  setBotId(botId: string): void {
+    this.botId = botId;
+    this.configurationWeb.setAuthorId(botId);
+    this.agentRtm.setAuthorId(botId);
+  }
+
+  verifyWebhookSecret(secret: string) {
+    return this.cfg.webhookSecret === secret;
+  }
+}
