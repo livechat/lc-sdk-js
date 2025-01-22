784d1cf96b93e2c4c99638bc649a16d9147fdc63
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2025-01-22 11:57:04 +0100

    get region from /v2/organizations/my

--- src/authorization/personal_access_token.ts
+++ src/authorization/personal_access_token.ts
@@ -2,15 +2,13 @@ import axios, { AxiosError } from "axios";
 import { TokenGetter } from "./token_getter";
 
 const globalAccountsURL = "https://accounts.livechat.com";
-const globalMapperURL = "https://api.text.com/globalmapper";
 
 export async function tokenGetterFromPAT(accountID: string, token: string): Promise<TokenGetter> {
-  const organizationID = await getOrganizationForPAT(accountID, token);
-  const region = await getRegionForOrganization(organizationID);
+  const { organization_id, region } = await getOrganizationForPAT(accountID, token);
 
   return () => ({
     accessToken: btoa(`${accountID}:${token}`),
-    organizationID: organizationID,
+    organizationID: organization_id,
     region: region,
     tokenType: "Basic",
   });
@@ -18,13 +16,14 @@ export async function tokenGetterFromPAT(accountID: string, token: string): Prom
 
 interface ResponseOrganization {
   organization_id: string;
+  region: string;
 }
 
 interface ResponseOrganizationError {
   error_description: string;
 }
 
-async function getOrganizationForPAT(accountID: string, token: string): Promise<string> {
+async function getOrganizationForPAT(accountID: string, token: string): Promise<ResponseOrganization> {
   const encodedData = btoa(`${accountID}:${token}`);
   const authorizationHeader = `Basic ${encodedData}`;
 
@@ -32,34 +31,13 @@ async function getOrganizationForPAT(accountID: string, token: string): Promise<
     const response = await axios.get<ResponseOrganization>(`${globalAccountsURL}/v2/organizations/my`, {
       headers: { Authorization: authorizationHeader },
     });
-    return response.data.organization_id;
+    return response.data;
   } catch (e) {
     if (isAxiosError<ResponseOrganizationError>(e) && e.response) {
       const errorMessage = e.response?.data.error_description ?? e;
       throw new Error(`Unable to get organization for PAT: ${errorMessage}`);
     }
     throw new Error(`Unable to get organization for PAT: ${e}`);
-  }
-}
-
-interface ResponseRegion {
-  region: string;
-}
-
-interface ResponseRegionError {
-  error_message: string;
-}
-
-async function getRegionForOrganization(organizationID: string): Promise<string> {
-  try {
-    const response = await axios.get<ResponseRegion>(`${globalMapperURL}/organization_id/${organizationID}/region`);
-    return response.data.region;
-  } catch (e) {
-    if (isAxiosError<ResponseRegionError>(e) && e.response) {
-      const errorMessage = e.response?.data.error_message ?? e;
-      throw new Error(`Unable to get region for organization ${organizationID}: ${errorMessage}`);
-    }
-    throw new Error(`Unable to get region for organization ${organizationID}: ${e}`);
   }
 }
 
