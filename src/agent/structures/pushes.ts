e532e638fce89860742ab07c7e179719d5a1e018
Sebastian Slimak <s.slimak@text.com>
2026-01-14 09:11:19 +0100

    chore: remove customer_unfollowed push ME-1234 (#187)

--- src/agent/structures/pushes.ts
+++ src/agent/structures/pushes.ts
@@ -196,11 +196,6 @@ export enum Pushes {
   CustomerLeft = "customer_left",
 
   /**
-   * Informs that a customer was unfollowed.
-   */
-  CustomerUnfollowed = "customer_unfollowed",
-
-  /**
    * Informs that an agent has been created.
    */
   AgentCreated = "agent_created",
