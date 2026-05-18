c51ea0b1840de1ec746d8413022fbcd850064333
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2026-05-18 09:16:30 +0200

    feat: add address to customer struct and methods v3.7 EUEE-1363 (#221)

--- src/agent/structures/structures.ts
+++ src/agent/structures/structures.ts
@@ -101,6 +101,14 @@ export interface Twilio {
   phone_number: string;
 }
 
+export interface Address {
+  address?: string;
+  city?: string;
+  country?: string;
+  state?: string;
+  postal_code?: string;
+}
+
 export interface CustomerChat {
   chat_id: string;
   thread_id?: string;
@@ -213,6 +221,7 @@ export interface CustomerParameters {
   avatar?: string;
   session_fields?: object[];
   phone_number?: string;
+  address?: Address;
 }
 
 export interface MulticastRecipients {
